76bacd658cc3a585b94951da1865991deefb8d82
Hasse <imberg.hasse@pm.me>
2024-04-23 15:26:59 +0300

    Project setup

---
+++ lib/models/time_block.dart
@@ -0,0 +1,11 @@
+class TimeBlock {
+  String blockName;
+  DateTime startTime;
+  DateTime endTime;
+
+  TimeBlock({
+    required this.blockName,
+    required this.startTime,
+    required this.endTime,
+  });
+}
