a11103d76567a5f969688aafcf24425018945c33
Hasse <imberg.hasse@pm.me>
2024-08-03 11:32:51 +0300

    open block editing state updates working

--- lib/screens/open_block.dart
+++ lib/screens/open_block.dart
@@ -1,33 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:time_blocking/dialogs/add_block.dart';
 import 'package:time_blocking/dialogs/confirm_dialog.dart';
+import 'package:time_blocking/storage/load_time_blocks.dart';
 
 class OpenBlockScreen extends StatefulWidget {
   const OpenBlockScreen(
-      this.currentBlock, this.index, this.removeBlock, this.updateState,
+      this.currentBlock, this.index, this.removeBlock, this.updateParentState,
       {super.key});
 
   final Map<String, dynamic> currentBlock;
   final int index;
   final Function(dynamic) removeBlock;
-  final Function() updateState;
+  final Function() updateParentState;
 
   @override
   OpenBlockScreenState createState() => OpenBlockScreenState();
 }
 
 class OpenBlockScreenState extends State<OpenBlockScreen> {
-  get currentBlock => widget.currentBlock;
-  get removeBlock => widget.removeBlock;
-  get index => widget.index;
-  get updateState => widget.updateState;
+  late Map<String, dynamic> _currentBlock;
+  Function get removeBlock => widget.removeBlock;
+  int get index => widget.index;
+  Function get updateParentState => widget.updateParentState;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentBlock = widget.currentBlock;
+  }
+
+  void updateState() {
+    loadTimeBlocks().then((updatedBlocks) {
+      setState(() {
+        _currentBlock = updatedBlocks[index];
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          currentBlock["blockName"],
+          _currentBlock["blockName"],
         ),
         actions: [
           IconButton(
@@ -42,7 +57,7 @@ class OpenBlockScreenState extends State<OpenBlockScreen> {
           IconButton(
             onPressed: () {
               // init will be triggered, so empty state update function
-              confirmDialog(context, updateState, action: () {
+              confirmDialog(context, updateParentState, action: () {
                 removeBlock(index);
                 Navigator.of(context).pop();
               },
