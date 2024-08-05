0cbeca5e3486c5c920d62a4eaa47be9587b22415
Hasse <imberg.hasse@pm.me>
2024-08-05 14:51:47 +0300

    description and toDo list added to block model

--- lib/models/time_block.dart
+++ lib/models/time_block.dart
@@ -4,10 +4,13 @@ class TimeBlock {
   String blockName;
   TimeOfDay startTime;
   TimeOfDay endTime;
+  String? description;
+  List<String>? toDo;
 
   TimeBlock({
     required this.blockName,
     required this.startTime,
     required this.endTime,
+    this.description,
   });
 }
